36ce67b81fcf29ce2f4b63718e9f60b1e486c47e
Sawan Kumar Jangra <98251229+sawanx0@users.noreply.github.com>
2023-07-30 23:36:54 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -121,7 +121,7 @@ const Contact = () => {
             type='submit'
             className='bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary'
           >
-            {loading ? "Sending..." : "Send"}
+            {loading ? "Sending..." : "Hit me up!"}
           </button>
         </form>
       </motion.div>
